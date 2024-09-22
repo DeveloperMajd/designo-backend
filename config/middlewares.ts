8db0a77058034fe0344b0a88802196ae18291406
DeveloperMajd <developermajd@gmail.com>
2024-09-22 17:15:47 +0200

    reset the middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,38 +1,12 @@
 export default [
-  "strapi::logger",
-  "strapi::errors",
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "script-src": ["https://cdnjs.cloudflare.com"],
-          "media-src": [
-            "https://cdnjs.cloudflare.com",
-            "data:",
-            "blob:",
-            "filesystem:",
-            "mediastream:",
-            "http://localhost:1337",
-          ],
-          "img-src": [
-            "https://cdnjs.cloudflare.com",
-            "data:",
-            "blob:",
-            "filesystem:",
-            "http://localhost:1337",
-          ],
-        },
-      },
-    },
-  },
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
