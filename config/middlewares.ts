470ded6dba7d6ee803ff16bbeb2d7b1185008c53
developermajd <developermajd@gmail.com>
2025-06-12 12:19:48 +0000

    edit middlewares

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -6,6 +6,8 @@ export default [
     config: {
       origin: [
         'https://designo-frontend.vercel.app',   
+        'https://designo-backend.developermajd.com',
+ 	'https://designo-frontend.developermajd.com',
       ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       headers: '*',
